6a2cd5c9a6e98be601d1c864ba1f44626c9bf76d
Alemán001 <cristianaleman888@gmail.com>
2022-12-06 09:48:20 -0600

    Archivos

---
+++ Pagina1.php
@@ -0,0 +1,11 @@
+<form method="post"
+action="Pagina2.php">
+por favor, envia la newsletter a: <br/>
+tu nombre: <input type="text"
+name="nombre"/><br/>
+tu apellido: <input type="text"
+name="apellido"/><br/>
+tu correo electrónico:<input
+type="text" name="email" /><br/>
+<input type="submit" value="Enviar formulario"/>
+</form>
